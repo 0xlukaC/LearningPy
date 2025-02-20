fe9beb93220c3e1f5ae1ea027fcaedf85b21ed69
0xlukaC <luka.chalker@gmail.com>
2025-02-20 23:00:28 +1100

    updated file exceptions

--- file_exceptions.py
+++ file_exceptions.py
@@ -7,25 +7,22 @@ try:
     with open("data.txt", "r") as f:
         print(f.read())
         print("number of lines", len(f.readlines()))
-except fileNotFoundError as e:
+except FileNotFoundError as e:
     print("error ", e)
 
 
 try:
     with open("user_output.txt", "w") as f:
         f.write(input("input stuff "))
-except as e :
+except Exception as e:
     print(e)
 
 try:
     with open("data.txt", "r") as f:
         with open("output.txt", "w") as o:
             o.write(f.read())
-except as e:
-    print("error: " e)
-
-
-
+except Exception as e:
+    print("error:", e)
 
 
 lines = []
@@ -38,4 +35,3 @@ while True:
         f.write(line)
 
 print(lines)
-
